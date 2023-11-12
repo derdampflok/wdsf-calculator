d155b4d4559c0d9d13791c6a56452f9081113c96
Dominik Depner <dominik.depner@isento.de>
2023-11-12 12:24:39 +0100

    simplify modifiers

--- main.py
+++ main.py
@@ -1,92 +1,75 @@
 from tables import *
 
 
-class Modifier:
-    def __init__(self, positive, percentage):
-        self.positive = positive
-        self.percentage = percentage
-
-
 def determine_number_of_couples_modifier(n):
-    if n < 24:
-        positive = False
-    else:
-        positive = True
 
     if n < 7:
-        percentage = 30
+        return -30
     elif n < 12:
-        percentage = 20
+        return -20
     elif n < 24:
-        percentage = 10
+        return -10
     elif n < 36:
-        percentage = 0
+        return 0
     elif 36 <= n < 48:
-        percentage = 10
+        return 10
     elif n < 96:
-        percentage = 25
+        return 25
     elif n < 144:
-        percentage = 30
+        return 30
     elif n < 192:
-        percentage = 35
+        return 35
     elif n < 288:
-        percentage = 40
+        return 40
     else:
-        percentage = 50
-
-    return Modifier(positive, percentage)
+        return 50
 
 
 def determine_top_12_modifier(n):
-    percentage = 0
     if n > 9:
-        percentage = 70
+        return 70
     elif n > 6:
-        percentage = 50
-    return Modifier(True, percentage)
+        return 50
+    return 0
 
 
 def determine_top_24_modifier(n):
-    percentage = 0
     if n > 9:
-        percentage = 35
-    elif n > 6:
-        percentage = 25
-    return Modifier(True, percentage)
+        return 35
+    if n > 6:
+        return 25
+    return 0
 
 
 def determine_top_36_modifier(n):
-    percentage = 0
     if n > 6:
-        percentage = 10
-    return Modifier(True, percentage)
+        return 10
+    return 0
 
 
 def determine_countries_modifier(n):
-    percentage = 0
     if n > 23:
-        percentage = 50
-    elif n > 15:
-        percentage = 40
-    elif n > 10:
-        percentage = 25
-    elif n > 7:
-        percentage = 10
-    return Modifier(True, percentage)
+        return 50
+    if n > 15:
+        return 40
+    if n > 10:
+        return 25
+    if n > 7:
+        return 10
+    return 0
 
 
 # wtf
 def determine_judge_modifier(n_judges, n_countries):
-    percentage = 0
     if n_countries > 10:
-        percentage = 30
-    elif n_countries > 8:
-        percentage = 25
-    elif n_judges == 9 & n_countries == 8:
-        percentage = 15
-    elif n_countries > 6:
-        percentage = 15
-    return Modifier(True, percentage)
+        return 30
+    if n_countries > 8:
+        return 25
+    if n_judges == 9 & n_countries == 8:
+        return 15
+    if n_countries > 6:
+        return 15
+    return 0
 
 
 def get_base_table_for_comp_type(comp_type):
@@ -104,30 +87,21 @@ def apply_modifiers(base_table, modifiers):
 
 
 if __name__ == '__main__':
-    compType = 0
-    numberOfCouples = 0
+    compType = 2
+    numberOfCouples = 21
     numberOfCouplesTop12 = 0
     numberOfCouplesTop24 = 0
-    numberOfCouplesTop36 = 0
-    coupleCountries = 0
-    numberOfJudges = 0
-    judgeCountries = 0
-
-    numberOfCouplesModifier = determine_number_of_couples_modifier(numberOfCouples)
-    top12Modifier = determine_top_12_modifier(numberOfCouplesTop12)
-    top24Modifier = determine_top_24_modifier(numberOfCouplesTop24)
-    top36Modifier = determine_top_36_modifier(numberOfCouplesTop36)
-    countriesModifier = determine_countries_modifier(coupleCountries)
-    judgeModifier = determine_judge_modifier(numberOfJudges, judgeCountries)
-
-    modifierList = [
-        numberOfCouplesModifier,
-        top12Modifier,
-        top24Modifier,
-        top36Modifier,
-        countriesModifier,
-        judgeModifier
-    ]
+    numberOfCouplesTop36 = 1
+    coupleCountries = 6
+    numberOfJudges = 11
+    judgeCountries = 10
+    
+    modifierList = [determine_number_of_couples_modifier(numberOfCouples),
+                    determine_top_12_modifier(numberOfCouplesTop12),
+                    determine_top_24_modifier(numberOfCouplesTop24),
+                    determine_top_36_modifier(numberOfCouplesTop36),
+                    determine_countries_modifier(coupleCountries),
+                    determine_judge_modifier(numberOfJudges, judgeCountries)]
 
     baseTable = get_base_table_for_comp_type(compType)
 
